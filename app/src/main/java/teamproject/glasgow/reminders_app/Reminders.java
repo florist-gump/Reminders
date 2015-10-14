404bb1d76a69c227e14703cfbbf20429a2d6d4c4
Flo <florian.deuerlein@gmail.com>
2015-10-14 19:35:00 +0100

    Basic reminders list implemented

--- app/src/main/java/teamproject/glasgow/reminders_app/Reminders.java
+++ app/src/main/java/teamproject/glasgow/reminders_app/Reminders.java
@@ -8,6 +8,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
+
+import org.joda.time.LocalTime;
+
+import java.util.ArrayList;
+
+import Helpers.DAYSOFTHEWEEK;
+import Model.*;
+import RemindersView.ExpandListAdapter;
 
 public class Reminders extends AppCompatActivity {
 
@@ -26,6 +35,25 @@ public class Reminders extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Reminder reminder = new Reminder("Teeth");
+        Occurrence occurrence1 = new Occurrence(DAYSOFTHEWEEK.FRIDAY,new LocalTime(12,2),reminder);
+        Occurrence occurrence2 = new Occurrence(DAYSOFTHEWEEK.WEDNESDAY,new LocalTime(12,50),reminder);
+        Occurrence occurrence3 = new Occurrence(DAYSOFTHEWEEK.MONDAY,new LocalTime(11,2),reminder);
+        Occurrence occurrence4 = new Occurrence(DAYSOFTHEWEEK.MONDAY,new LocalTime(10,2),reminder);
+
+        reminder.addOccurrence(occurrence1);
+        reminder.addOccurrence(occurrence2);
+        reminder.addOccurrence(occurrence3);
+        reminder.addOccurrence(occurrence4);
+
+        Model.Reminders reminders = new Model.Reminders();
+        reminders.addReminder(reminder);
+
+        ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.ExpList);
+        ExpandListAdapter ExpAdapter = new ExpandListAdapter(Reminders.this, reminders);
+        expandableListView.setAdapter(ExpAdapter);
+
     }
 
     @Override
